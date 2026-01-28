4cc15c31b0335a1941b432ab59d1babf0213683c
MD <md@noreply.homelab>
2026-01-28 20:38:35 +0000

    modified subprocess command to actually output

--- src/monitor.py
+++ src/monitor.py
@@ -1,4 +1,5 @@
 import subprocess
+import json
 
 def get_download_speed(download_info):
     bytes = download_info.bytes
@@ -12,7 +13,8 @@ def get_upload_speed(upload_info):
     
 
 def get_internet_speed():
-    output = subprocess.run(["speedtest", "--format=json"])
+    output_bytes = subprocess.run(["speedtest", "--format=json"], capture_output=True).stdout
+    output = json.loads(output_bytes.decode('utf-8'))
     download_speed = get_download_speed(output.download)
     upload_speed = get_upload_speed(output.upload)
     return (download_speed, upload_speed)
